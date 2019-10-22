e50b770980585ce1c969e80446d7880319f9604f
Julien Calixte <julienc@bam.tech>
2019-10-22 23:26:15 +0200

    :sparkles: (Generation) Generate all png images from source
    Add a manifest.json with icons attribute.

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,63 @@
 #!/usr/bin / env node
 const yargs = require("yargs");
+const sharp = require("sharp");
+const fs = require('fs');
 
 const options = yargs
-  .usage("Usage: -a <asset> -o <folder_output>")
+  .usage("Usage: -a <asset> -o <output>")
   .option("a", { alias: "asset", describe: "A PNG file", type: "string", demandOption: true })
-  .option("o", { alias: "folder", describe: "folder output", type: "string" })
+  .option("o", { alias: "output", describe: "folder output", type: "string" })
   .argv;
 
-const greeting = `Hello, ${options.name}!`;
+const information = `Generating images for ${options.asset}!`;
+console.log(information);
 
-console.log(greeting);
+const icons = [];
+
+const outputFolder = (options.output || '').trim('/') || '.'
+
+const resize = (name, width, height = undefined, showSize = true) => {
+  if (!height) {
+    height = width
+  }
+
+  const size = showSize ? `-${width}x${height}` : ''
+
+  sharp(options.asset)
+    .resize(width, height, {
+      background: 'transparent',
+      fit: 'contain'
+    })
+    .toFile(`./${outputFolder}/${name}${size}.png`, function (err) {
+      if (err) {
+        console.error(err)
+      }
+    });
+  icons.push({
+    "src": `./img/icons/${name}${size}.png`,
+    "sizes": `${width}x${height}`,
+    "type": "image/png"
+  });
+};
+
+resize('android-chrome', 192);
+resize('android-chrome', 512);
+resize('apple-touch-icon', 60);
+resize('apple-touch-icon', 76);
+resize('apple-touch-icon', 120);
+resize('apple-touch-icon', 152);
+resize('apple-touch-icon', 180);
+resize('apple-touch-icon', 180, 180, false);
+resize('favicon', 16);
+resize('favicon', 32);
+resize('msapplication-icon', 144);
+resize('mstile', 150);
+
+const json = JSON.stringify({ icons }, null, 2)
+
+fs.writeFile(`./${outputFolder}/manifest.json`, json, function (err) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("Manifest file is created!");
+}); 
