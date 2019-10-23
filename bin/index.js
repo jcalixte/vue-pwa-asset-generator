c174fffd4cc05cf9c9bd59e765f1f590f048f0a5
Julien Calixte <julienc@bam.tech>
2019-10-23 08:45:17 +0200

    :sparkles: (Output folder) Create output folder if it does not exist

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
-
 const yargs = require("yargs");
 const sharp = require("sharp");
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 const options = yargs
   .usage("Usage: -a <asset> -o <output>")
-  .option("a", { alias: "asset", describe: "A PNG file", type: "string", demandOption: true })
+  .option("a", {
+    alias: "asset",
+    describe: "A PNG file",
+    type: "string",
+    demandOption: true
+  })
   .option("o", { alias: "output", describe: "folder output", type: "string" })
   .argv;
 
@@ -15,50 +20,55 @@ console.log(information);
 
 const icons = [];
 
-const outputFolder = (options.output || '').trim('/') || '.'
+const outputFolder = path.normalize(options.output || "");
+const absoluteOutput = path.resolve(outputFolder);
 
-const resize = (name, width, height = undefined, showSize = true) => {
+if (!fs.existsSync(absoluteOutput)) {
+  fs.mkdirSync(absoluteOutput);
+}
+
+const resize = (name, width, height = undefined, displaySize = true) => {
   if (!height) {
-    height = width
+    height = width;
   }
 
-  const size = showSize ? `-${width}x${height}` : ''
+  const size = displaySize ? `-${width}x${height}` : "";
 
   sharp(options.asset)
     .resize(width, height, {
-      background: 'transparent',
-      fit: 'contain'
+      background: "transparent",
+      fit: "contain"
     })
-    .toFile(`./${outputFolder}/${name}${size}.png`, function (err) {
+    .toFile(`${absoluteOutput}/${name}${size}.png`, function(err) {
       if (err) {
-        console.error(err)
+        console.error(err);
       }
     });
   icons.push({
-    "src": `./img/icons/${name}${size}.png`,
-    "sizes": `${width}x${height}`,
-    "type": "image/png"
+    src: `./img/icons/${name}${size}.png`,
+    sizes: `${width}x${height}`,
+    type: "image/png"
   });
 };
 
-resize('android-chrome', 192);
-resize('android-chrome', 512);
-resize('apple-touch-icon', 60);
-resize('apple-touch-icon', 76);
-resize('apple-touch-icon', 120);
-resize('apple-touch-icon', 152);
-resize('apple-touch-icon', 180);
-resize('apple-touch-icon', 180, 180, false);
-resize('favicon', 16);
-resize('favicon', 32);
-resize('msapplication-icon', 144);
-resize('mstile', 150);
+resize("android-chrome", 192);
+resize("android-chrome", 512);
+resize("apple-touch-icon", 60);
+resize("apple-touch-icon", 76);
+resize("apple-touch-icon", 120);
+resize("apple-touch-icon", 152);
+resize("apple-touch-icon", 180);
+resize("apple-touch-icon", 180, 180, false);
+resize("favicon", 16);
+resize("favicon", 32);
+resize("msapplication-icon", 144);
+resize("mstile", 150);
 
-const json = JSON.stringify({ icons }, null, 2)
+const json = JSON.stringify({ icons }, null, 2);
 
-fs.writeFile(`./${outputFolder}/manifest.json`, json, function (err) {
+fs.writeFile(`./${outputFolder}/manifest.json`, json, function(err) {
   if (err) {
     return console.error(err);
   }
   console.log("Manifest file is created!");
-}); 
+});
